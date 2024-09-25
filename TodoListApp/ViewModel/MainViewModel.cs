87cff5da65b018200d99d292caa75acf7ff5d10c
bbangwon <bbangwon.chung@gmail.com>
2024-09-26 07:28:59 +0900

    일반적인 MVVM 방식

---
+++ TodoListApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace TodoListApp.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    string? text;
+    public string? Text
+    {
+        get => text;
+        set
+        {
+            text = value;
+            OnPropertyChanged(nameof(Text));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
+
